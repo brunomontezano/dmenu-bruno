df733d9a52033b76c36a563600d308898150ce6b
Bruno Montezano <brunodoyt@gmail.com>
2020-12-07 21:21:13 -0300

    Making colors more consistent.

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const char *prompt      = NULL;      /* -p option; prompt to the left of 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#cccccc", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#595959" },
+	[SchemeSel] = { "#ffffff", "#434758" },
 	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
 	[SchemeNormHighlight] = { "#AAAAAA", "#000000" },
 	[SchemeOut] = { "#000000", "#00ffff" },
