4395c1c24bd83e3c92a232bd57b805e60b5dc3c5
Bruno Montezano <brunodoyt@gmail.com>
2021-07-14 00:55:42 -0300

    Change colorscheme.

--- config.def.h
+++ config.def.h
@@ -18,21 +18,21 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
 
-static const char nord_fg[]         = "#D8DEE9";
-static const char nord_bg[]         = "#2E3440";
-static const char nord_blue[]       = "#81A1C1";
-static const char nord_grey[]       = "#3B4252";
-static const char nord_yellow[]     = "#EBCB8B";
-static const char nord_white[]      = "#E5E9F0";
+static const char tn_fg[]         = "#c0caf5";
+static const char tn_bg[]         = "#1a1b26";
+static const char tn_blue[]       = "#7aa2f7";
+static const char tn_grey[]       = "#414868";
+static const char tn_yellow[]     = "#e0af68";
+static const char tn_white[]      = "#c0caf5";
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { nord_fg, nord_bg   },
-	[SchemeSel] =  { nord_bg, nord_blue },
-	[SchemeMid] = { nord_white, nord_grey },
-	[SchemeOut] =  { nord_fg, nord_bg   },
-    [SchemeSelHighlight] = { nord_yellow, nord_bg },
-	[SchemeNormHighlight] = { nord_yellow, nord_grey },
+	[SchemeNorm] = { tn_fg, tn_bg   },
+	[SchemeSel] =  { tn_bg, tn_blue },
+	[SchemeMid] = { tn_white, tn_grey },
+	[SchemeOut] =  { tn_fg, tn_bg   },
+    [SchemeSelHighlight] = { tn_yellow, tn_bg },
+	[SchemeNormHighlight] = { tn_yellow, tn_grey },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
